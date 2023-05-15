94c4c0ebd5673afd1760086991a2103cb673bee2
zerodayz <rcernin@redhat.com>
2023-05-15 19:33:04 -0400

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -5,12 +5,14 @@ from ibm_cloud_sdk_core.api_exception import ApiException
 class TestEnglishToFrench(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(english_to_french('Bonjour'), 'Hello')
     def test_null_input(self):
         self.assertEqual(english_to_french(''), None)
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertNotEqual(french_to_english('Hello'), 'Bonjour')
     def test_null_input(self):
         self.assertEqual(french_to_english(''), None)
 
